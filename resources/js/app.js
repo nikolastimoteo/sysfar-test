b35eae5272e43741b3fbe3be727c64e1b038527d
Nikolas Timoteo <nikolastps@hotmail.com>
2020-06-18 11:22:51 -0300

    Instalação e configuração do VueAxios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,25 @@
 require('./bootstrap');
 import Vue from 'vue';
+import VueAxios from 'vue-axios';
 import App from './components/App.vue';
 import router from './router';
+
+// Axios Config
+const axiosInstance = axios.create({
+    baseURL: '/api/v1',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+});
+Vue.use(VueAxios, axiosInstance);
+
+// Axios Interceptors
+Vue.axios.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('user-token');
+    config.headers.Authorization =  token ? `Bearer ${token}` : '';
+    return config;
+});
 
 new Vue({
     el: '#app',
