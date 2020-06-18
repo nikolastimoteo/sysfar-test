5cb1aee89a8a48745f3c9f7838dfccd4c98dc605
Nikolas Timoteo <nikolastps@hotmail.com>
2020-06-18 11:16:22 -0300

    Criação do seeder para a tabela de usuários

--- app/Http/Controllers/API/v1/AuthController.php
+++ app/Http/Controllers/API/v1/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\v1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\API\v1\LoginFormRequest;
+use JWTAuth;
 
 class AuthController extends Controller
 {
