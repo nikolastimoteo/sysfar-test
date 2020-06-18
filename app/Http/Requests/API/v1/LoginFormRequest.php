309a46eb5b1c493eccd024394b8ac65eab0b25a2
Nikolas Timoteo <nikolastps@hotmail.com>
2020-06-18 10:35:12 -0300

    Criação do request para o formulário de login e do resource de usuário + Definição da timezone da aplicação

---
+++ app/Http/Requests/API/v1/LoginFormRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\API\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email'          => 'required|email',
+            'password'       => 'required'
+        ];
+    }
+
+    /**
+     * Get the messages to apply to the validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'required'      => 'Campo obrigatório.',
+            'email'         => 'E-mail inválido.'
+        ];
+    }
+}
